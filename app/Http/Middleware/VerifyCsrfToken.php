2d0e5fe4f47def68b743f7f9a806e10a84541227
artanila <aplikasinilaarta@gmail.com>
2021-06-03 17:37:53 +0800

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'midtrans.com', 'http://postman.com/*'
+        'midtrans.com', 'http://web.postman.com/*'
     ];
 }
