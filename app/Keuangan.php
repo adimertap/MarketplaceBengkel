8f4005df9a7aa82575fa7c739853398573dac72e
artanila <aplikasinilaarta@gmail.com>
2022-01-11 13:57:24 +0800

    Update Keuangan.php

--- app/Keuangan.php
+++ app/Keuangan.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Keuangan extends Model
 {
-     protected $table = "tb_marketplace_keuangan";
+    protected $table = "tb_marketplace_keuangan";
 
     protected $primaryKey = 'id_keuangan';
     protected $fillable = [
-       'id_bengkel', 'jumlah', 'status', 'keterangan'];
+       'id_bengkel', 'jumlah', 'status', 'keterangan', 'id_bank_account', 'photo'];
 
        public function Bengkel(){
         return $this->belongsTo(Bengkel::class, 'id_bengkel', 'id_bengkel');
